9d4289034c1bba136e652df47c223bac7530688c
eloriummusic <elorium@eloriummusic.com>
2026-04-29 17:17:19 +1000

    subscribe.js

--- api/subscribe.js
+++ api/subscribe.js
@@ -1,6 +1,6 @@
-export default async function handler(req, res) {
+module.exports = async (req, res) => {
   if (req.method !== "POST") {
-    return res.status(405).end();
+    return res.status(405).json({ error: "Method not allowed" });
   }
 
   const { email } = req.body;
@@ -24,11 +24,11 @@ export default async function handler(req, res) {
       })
     });
 
-    const data = await response.json();
+    await response.json();
 
     return res.status(200).json({ success: true });
 
   } catch (err) {
     return res.status(500).json({ error: "server error" });
   }
-}
+};
